6dc50ab704ce9fa02ccbb52ba17e56d93ce12703
vcaruso <vcaruso@xeniaprogetti.it>
2024-01-03 12:10:53 +0100

    Complete Shortly

--- shortly/js/script.js
+++ shortly/js/script.js
@@ -1,11 +1,42 @@
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+
 const input = document.getElementById('link-input');
 const linkForm = document.getElementById('link-form');
 const errMsg = document.getElementById('err-msg');
 
+btn.addEventListener('click',navToggle);
 linkForm.addEventListener('submit', formSubmit);
 
+// Toggle Mobile Menu
+function navToggle(){
+    btn.classList.toggle('open');
+    menu.classList.toggle('flex');
+    menu.classList.toggle('hidden');
+}
+
+function validUrl(str) {
+    var urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
+  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
+  '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
+  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
+  '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
+  '(\\#[-a-z\\d_]*)?$','i'); // validate fragment locator
+  return !!urlPattern.test(str);
+}
 
 function formSubmit(e){
     e.preventDefault();
-    console.log('123');
+    if(input.value === ''){
+        errMsg.innerHTML = 'Please enter something';
+        input.classList.add('border-red');
+    } else if(! validUrl(input.value)){
+        errMsg.innerHTML = 'Please enter a valid URL';
+        input.classList.add('border-red');
+    } else {
+        errMsg.innerHTML = '';
+        input.classList.remove('border-red');
+        alert('success');
+    }
+
 }
